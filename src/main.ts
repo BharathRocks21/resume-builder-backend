d202b47bacb06b8c143eaff6eebd09196192bd29
BharathRocks21 <bbcc010151@gmail.com>
2025-05-26 22:23:34 +0530

    commit of main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: 'https://bharathrocks21.github.io',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
-
-
